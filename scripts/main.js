6aceff0981d8929eaef62649e792c1e509a903e1
DragonDrew15 <aalcantaratorres@outlook.com>
2022-07-26 18:18:34 -0500

    se alimenta el carrusel con informacion desde TMDB API

--- scripts/main.js
+++ scripts/main.js
@@ -25,3 +25,53 @@ flechaIzquierda.addEventListener('click', () => {
 		indicadorActivo.classList.remove('activo');
 	}
 });
+
+
+const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+const carousel1 = document.getElementById('carousel1');
+
+getMovies(API_URL);
+
+function getMovies(url){
+	fetch(url).then(res => res.json()).then(data => {
+		// console.log(data);
+		showMovies(data.results)
+	})
+}
+
+function showMovies(data){
+	carousel1.innerHTML = '';
+
+	data.forEach(movie => {
+		const {title, poster_path, overview} = movie;
+		const movieElem = document.createElement('div');		
+		movieElem.classList.add('pelicula');
+		movieElem.innerHTML = `
+			<a href="#"><img src="${IMG_URL+poster_path}" alt="${title}"></a>
+			<h4 class= "bg-black text-light">${title}</h4>
+		`
+
+	carousel1.appendChild(movieElem);
+	});
+}
+
+// 			`
+// 			<img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+// 	   		<div class="movie-info">
+// 		   		<h3>${title}</h3>
+// 		   		<span class="${getColor(vote_average)}">${vote_average}</span>
+// 	   		</div>
+
+// 	   		<div class="overview">
+
+// 		   		<h3>Overview</h3>
+// 		   		${overview}
+// 		   		<br/> 
+// 		   		<button class="know-more" id="${id}">Know More</button
+// 	   		</div>
+   
+//    		`
