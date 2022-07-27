cfde9faec2e86ec1bf597964868ff982ea3e0907
Nayeli Blas Garcia <nayeblas@gmail.com>
2022-07-27 14:38:46 -0500

    Agregando cuadro descriptivo

--- scripts/main.js
+++ scripts/main.js
@@ -39,7 +39,7 @@ getMovies(API_URL);
 function getMovies(url){
 	fetch(url).then(res => res.json()).then(data => {
 		// console.log(data);
-		showMovies(data.results)
+		//showMovies(data.results)
 	})
 }
 
