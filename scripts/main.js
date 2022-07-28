6d15b4251f9f1b70e79c98595853278bc9177b33
philter15 <64454092+philter15@users.noreply.github.com>
2022-07-28 14:19:47 -0500

    overview oculto
    cambios para ocultar overview y que aparezcan con el cursor

--- scripts/main.js
+++ scripts/main.js
@@ -26,20 +26,27 @@ flechaIzquierda.addEventListener('click', () => {
 	}
 });
 
+////////////////////////////TMDB//////////////////////////////////
+ 
 
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+
+const API_KEY = '&api_key=580c12c0e5e7750e60a81c6bb5d745a1';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+// const TRENDING = BASE_URL + '/discover/movie?sort_by=popularity.desc&language=es'+API_KEY;
+const TRENDING = BASE_URL + '/trending/movie/week?' + API_KEY + '&language=es';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL+'/search/movie?'+API_KEY; 
 
 const carousel1 = document.getElementById('carousel1');
+const form = document.getElementById ('form');
+const search = document.getElementById ('search');
 
-getMovies(API_URL);
+getMovies(TRENDING);
 
 function getMovies(url){
 	fetch(url).then(res => res.json()).then(data => {
 		// console.log(data);
-		showMovies(data.results)
+		showMovies(data.results);
 	})
 }
 
@@ -47,31 +54,32 @@ function showMovies(data){
 	carousel1.innerHTML = '';
 
 	data.forEach(movie => {
-		const {title, poster_path, overview} = movie;
+		const {title, poster_path, overview, release_date} = movie;
 		const movieElem = document.createElement('div');		
 		movieElem.classList.add('pelicula');
-		movieElem.innerHTML = `
+		movieElem.innerHTML = `			
 			<a href="#"><img src="${IMG_URL+poster_path}" alt="${title}"></a>
-			<h4 class= "bg-black text-light">${title}</h4>
+			<div class="descripcion bg-light text-secondary text-justify">
+				<p class="title" id="title">${title}</p>
+				<p class="overview " id="overview"> "${overview}"</p>
+				<p class="title-cast font-weight-bold">Cast</p>
+				<p class="cast" id="cast">....</p>
+				<p class="title-release">Release Date</p>
+				<p class="release date" id="release date">"${release_date}"</p>
+			</div>
 		`
 
 	carousel1.appendChild(movieElem);
 	});
 }
 
-// 			`
-// 			<img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
-// 	   		<div class="movie-info">
-// 		   		<h3>${title}</h3>
-// 		   		<span class="${getColor(vote_average)}">${vote_average}</span>
-// 	   		</div>
-
-// 	   		<div class="overview">
-
-// 		   		<h3>Overview</h3>
-// 		   		${overview}
-// 		   		<br/> 
-// 		   		<button class="know-more" id="${id}">Know More</button
-// 	   		</div>
-   
-//    		`
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const searchTerm = search.value;
+    if(searchTerm){
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(TRENDING);
+    }
+    
+})
