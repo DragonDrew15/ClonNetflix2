12cb5de734e86bb27aea393d9097f664c489a142
DragonDrew15 <aalcantaratorres@outlook.com>
2022-07-28 12:28:55 -0500

    se ajusto el tamaño de los contenedores

--- scripts/main.js
+++ scripts/main.js
@@ -26,10 +26,13 @@ flechaIzquierda.addEventListener('click', () => {
 	}
 });
 
+////////////////////////////TMDB//////////////////////////////////
+ 
 
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+
+const API_KEY = '&api_key=1cf50e6248dc270629e802686245c2c8';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&language=es'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
 const carousel1 = document.getElementById('carousel1');
@@ -39,7 +42,7 @@ getMovies(API_URL);
 function getMovies(url){
 	fetch(url).then(res => res.json()).then(data => {
 		// console.log(data);
-		// showMovies(data.results);
+		showMovies(data.results);
 	})
 }
 
@@ -52,8 +55,8 @@ function showMovies(data){
 		movieElem.classList.add('pelicula');
 		movieElem.innerHTML = `			
 			<a href="#"><img src="${IMG_URL+poster_path}" alt="${title}"></a>
-			<p class="title" id="title">${title}</p>
-			<div class="descripcion bg-light text-secondary text-justify"  >
+			<div class="descripcion bg-light text-secondary text-justify">
+				<p class="title" id="title">${title}</p>
 				<p class="overview " id="overview"> "${overview}"</p>
 				<p class="title-cast font-weight-bold">Cast</p>
 				<p class="cast" id="cast">....</p>
